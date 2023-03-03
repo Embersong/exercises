968191aa852ccd4e6fdc57d5b6f0ae9974afa6be
Embersong <evebay@mail.ru>
2023-03-03 21:47:52 +0700

    Add files via upload

--- ответы/04.php
+++ ответы/04.php
@@ -5,3 +5,9 @@ for ($i = 4; $i >= 0; $i--) {
     $arr[$i * 2 + 1] = $arr[$i * 2] = $arr[$i];
 }
 print_r($arr);
+
+//2 вариант только если во второй половине чисто нули
+$arr =  ["str", 2, 3, 4, 5, 0, 0, 0, 0, 0];
+
+$newArr = array_diff($arr, array(0));
+print_r(array_merge(...array_map(null, $newArr, $newArr)));
